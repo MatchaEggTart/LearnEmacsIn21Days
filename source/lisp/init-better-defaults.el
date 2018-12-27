711c60bf19cdf800a5d1284ac1b7b665749528aa
matchaeggtart <matchaeggtart@hotmail.com>
2018-12-27 12:35:15 +0800

    Update

---
+++ source/lisp/init-better-defaults.el
@@ -0,0 +1,50 @@
+;; (setq ring-bell-function 'ignore)
+
+(global-auto-revert-mode t)
+
+(setq make-backup-files nil)
+(setq auto-save-default nil)
+
+(recentf-mode 1)
+(setq recentf-max-menu-items 25)
+
+(electric-pair-mode 1)
+(setq electric-pair-inhibit-predicate 'electric-pair-conservative-inhibit)
+(show-paren-mode t)
+
+;; paren
+(add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
+(define-advice show-paren-function (:around (fn) fix-show-paren-function)
+  "Highlight enclosing parens."
+  (cond ((looking-at-p "\\s(") (funcall fn))
+        (t (save-excursion
+             (ignore-errors (backward-up-list))
+             (funcall fn)))))
+
+;; (abbrev-mode t)
+;; (define-abbrev-table 'global-abbrev-table '(
+;;					    ("abbreviation""full name")
+;;					    ("abbreviation""full name")
+;;					    ))
+
+(delete-selection-mode t)	;; mark set
+
+;; Menu Chinese  
+(set-language-environment 'Chinese-GB)
+
+;; utf-8
+(setq default-buffer-file-coding-system 'utf-8)
+(prefer-coding-system 'utf-8)
+
+;; Dired Mode
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+(with-eval-after-load 'dired
+    (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+(require 'dired-x)
+
+
+(provide 'init-better-defaults)
---
+++ source/lisp/init-packages.el
@@ -0,0 +1,52 @@
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                           ("melpa" . "https://melpa.org/packages/"))))
+
+;; cl - Common Lisp Extension
+(require 'cl)
+
+;; Add Packages
+(defvar donald/packages '(
+			  ;; --- Auto-completion ---
+			  company
+			  popwin
+			  hungry-delete
+			  spacemacs-theme
+			  solarized-theme
+			  ;;smartparens
+			  iedit
+			  ) "Default packages")
+
+(setq package-selected-packages donald/packages)
+
+(defun donald/packages-installed-p ()
+  (loop for pkg in donald/packages
+        when (not (package-installed-p pkg)) do (return nil)
+        finally (return t)))
+
+(unless (donald/packages-installed-p)
+  (message "%s" "Refreshing package database...")
+  (package-refresh-contents)
+  (dolist (pkg donald/packages)
+    (when (not (package-installed-p pkg))
+      (package-install pkg))))
+
+
+
+;; (smartparens-global-mode t)
+
+(global-company-mode t)
+(setq-default company-idle-delay 0.2)
+(setq-default company-minimum-prefix-length 2)
+
+(global-hungry-delete-mode)
+
+(load-theme 'spacemacs-dark t)
+
+(require 'popwin)
+(popwin-mode 1)
+
+(require 'iedit)
+
+(provide 'init-packages)
